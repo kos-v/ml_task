44ed4a2ebf04770834847ac7a8b63e155f0ec449
Konstantin Voloshchuk <kosv.dev@gmail.com>
2021-06-27 16:23:22 +0600

    Configure app for sqlite

--- config/db.php
+++ config/db.php
@@ -2,11 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
+    'dsn' => 'sqlite:@app/app.db',
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
