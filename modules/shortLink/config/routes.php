057e6c46be153d41603b11d97b3b067adcd43cd9
Konstantin Voloshchuk <kosv.dev@gmail.com>
2021-06-28 13:16:45 +0600

    Add api implementation

--- modules/shortLink/config/routes.php
+++ modules/shortLink/config/routes.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-
+    'GET api/v1/link/<uid:[A-Za-z0-9]{6}>' => 'shortLink/api/v1/link/get',
+    'POST api/v1/link/create' => 'shortLink/api/v1/link/create',
 ];
---
+++ modules/shortLink/controllers/api/v1/LinkController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\modules\shortLink\controllers\api\v1;
+
+use Yii;
+use yii\helpers\Url;
+use yii\web\Response;
+
+class LinkController extends BaseController
+{
+    public function actionGet(string $uid): Response
+    {
+        $shortLink = $this->getLinkManager()->find($uid);
+        if (!$shortLink) {
+            return $this->responseNotFound('Link not found');
+        }
+        
+        return $this->response([
+            'originalLink' => $shortLink->link,
+            'openingsCount' => 0,
+        ]);
+    }
+    
+    public function actionCreate(): Response
+    {
+        $url = Yii::$app->request->post('url', '');
+        
+        if ($err = $this->getLinkManager()->validateLink($url)) {
+            return $this->responseNotValid(['url' => $err]);
+        }
+        
+        return $this->response([
+            'uid' => Url::base(true) . '/' . $this->getLinkManager()->create($url)
+        ]);
+    }
+}
